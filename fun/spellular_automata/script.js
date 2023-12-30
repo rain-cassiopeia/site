0d695bc210548af81010a15c64c3ee43ac36c548
Rain White <rwhite2@swarthmore.edu>
2023-12-30 14:45:19 -0500

    progress on spells. why is it so slow???

--- fun/spellular_automata/script.js
+++ fun/spellular_automata/script.js
@@ -1,5 +1,150 @@
+winX = 0; // window width
+winY = 0; // window height
+pixX = 0; // number of pixels wide 
+pixY = 0; // number of pixels tall
+pixList = []; // all pixels, live updated
+edgeList = []; // live edges, updated once per cycle
+nextEdgeList = []; // next cycle's live edges, accumulating
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const offscreenCanvas = document.createElement('canvas');
+const offscreenCtx = offscreenCanvas.getContext('2d');
 
-ctx.fillStyle = 'rgb(2, 200, 255)';
-ctx.fillRect(10, 10, 150, 100);
+class Pixel {
+    constructor(x, y, living) {
+        //x and y are NOT window coordinates, they are pixel coordinates!! 
+        //living is bool
+        this.x = x;
+        this.y = y;
+        this.alive = living;
+    }
+}
+class Edge extends Pixel{
+    constructor(x, y, color) {
+        //color is list of r, g, b.
+        super(x, y);
+        this.alive = true;
+        this.color = color;
+        // window coordinates! this works for 2x2 pixels, would change for different pixel size.
+        this.real_x = 2 * this.x;
+        this.real_y = 2 * this.y;
+    }
+    getFriends() { // all my neighbors are my friends :D
+        //returns list of lists of coordinates of living neighbors
+        //currently not doing wraparound. might add later
+        let friendList = [];
+        if (this.x > 0 && !pixList[(this.x - 1) + (pixX * this.y)].alive) { //left
+            friendList.push([this.x - 1, this.y]);
+        }
+        if (this.x < (pixX - 1) && pixList[(this.x + 1) + (pixX * this.y)]) { //right
+            friendList.push([this.x + 1, this.y]);
+        }
+        if (this.y > 0 && pixList[this.x + (pixX * (this.y - 1))]) { //top
+            friendList.push([this.x, this.y - 1]);
+        }
+        if (this.y < (pixY - 1) && pixList[this.x + (pixX * (this.y + 1))]) { //bottom
+            friendList.push([this.x, this.y + 1]);
+        }
+        return friendList;
+    }
+    tick() {
+        let friends = this.getFriends()
+        let numfriends = friends.length;
+        if (numfriends > 1) {
+            nextEdgeList.push(this); // this could only be a live edge next round if dead neighbors > 2
+            spawn(this.color, friends[Math.floor(Math.random() * numfriends)]);
+        }
+        else if (numfriends == 1) {
+            spawn(this.color, friends[0]);
+        }
+    }
+}
+
+function mutate(color) { // lots of ways to do this!! TODO: tweak :p
+    let newColor = [0, 0, 0]
+    for (let i=0; i<3; i++) {
+        if (Math.random() < (1/3)) { //tweak me!
+            if (color[i] == 255) {newColor[i] = 254}
+            else if (color[i] == 0) {newColor[i] = 1}
+            else {
+                if (Math.random() < 0.5) {
+                    newColor[i]++;
+                }
+                else {
+                    newColor[i]--;
+                }
+            }
+        }
+    }
+    return newColor;
+}
+
+function spawn(parentColor, [px, py]) {
+    pixList[px + pixX * py].alive = true; 
+    let newColor = mutate(parentColor);
+    let newPixel = new Edge(px, py, newColor);
+    nextEdgeList.push(newPixel); // im assuming it would be quicker to do this without checking but idk
+    offscreenCtx.fillStyle = `rgb(${newColor[0]}, ${newColor[1]}, ${newColor[2]})`;
+    offscreenCtx.fillRect(newPixel.real_x, newPixel.real_y, 2, 2); // 2x2!
+}
+
+function cycle() {
+    for (let i=0; i<edgeList.length; i++){
+        // tick every live edge
+        // this handles appending to nextEdgeList and drawing children to offscreenCanvas
+        edgeList[i].tick(); 
+    }
+    edgeList = nextEdgeList;
+    nextEdgeList = [];
+    ctx.drawImage(offscreenCanvas, 0, 0);
+}
+
+function onClick(event) {
+    setupCanvas()
+    let clickX = event.clientX / 2; // divide by 2 for 2x2 grid!
+    let clickY = event.clientY / 2;
+    spawn([200, 200, 200], [clickX, clickY]); // tweak with starting color?
+}
+
+function doWindowSize() {
+    winX = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    winY = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    pixX = winX / 2; // 2x2 pixels 
+    pixY = winY / 2;
+}
+function setupCanvas(){
+    doWindowSize()
+    canvas.width = winX;
+    offscreenCanvas.width = winX;
+    canvas.height = winY;
+    offscreenCanvas.height = winY;
+    offscreenCtx.clearRect(0, 0, winX, winY);
+
+    //populating the pixList array:
+    for (let i=0; i<pixY; i++) { 
+        for (let j=0; j<pixX; j++) {
+            pixList.push(new Pixel(i, j, false));
+        }
+    }
+}
+
+setupCanvas();
+setInterval(cycle, 1000);
+document.addEventListener("click", onClick)
+
+// ctx.fillStyle = 'rgb(2, 200, 255)';
+// ctx.fillRect(10, 10, 2, 2);
+// ctx.fillStyle = 'rgb(255, 0, 0)';
+// ctx.fillRect(161, 111, 1, 1);
+
+
+
+
+
+// const offscreenCanvas = document.createElement('canvas');
+// const offscreenCtx = offscreenCanvas.getContext('2d');
+
+// // Draw your objects on the offscreen canvas
+
+// // Draw the offscreen canvas onto the main canvas
+// ctx.drawImage(offscreenCanvas, 0, 0);
