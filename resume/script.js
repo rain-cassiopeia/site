d428a6d233ef5053486fa32137d8f98f76d9e02b
Rain White <rwhite2@swarthmore.edu>
2024-01-18 09:21:49 -0500

    fixed download button working

--- resume/script.js
+++ resume/script.js
@@ -1,9 +1,6 @@
 function downloadPDF() {
-    const pdfURL = "Rain White Resume-1.pdf";
-    const link = document.createElement('a');
-    link.href = pdfURL;
+    var link = document.createElement('a');
+    link.href = "Rain White Resume-1.pdf";
     link.download = "Rain White Resume.pdf";
-    document.body.appendChild(link);
     link.click();
-    document.body.removeChild(link);
 }
