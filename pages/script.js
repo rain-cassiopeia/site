32227a02023d4e9c305b129f000012eeae8fb6c5
Rain White <rwhite2@swarthmore.edu>
2024-07-05 23:09:15 -0400

    ran into a hard limit, decided to start by making a version just for me

---
+++ pages/script.js
@@ -0,0 +1,58 @@
+// The Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCOV4iAQziteXkvOjF6ZBlNFsYTYDwECSw",
+    authDomain: "raindocs-a679e.firebaseapp.com",
+    projectId: "raindocs-a679e",
+    storageBucket: "raindocs-a679e.appspot.com",
+    messagingSenderId: "25079591688",
+    appId: "1:25079591688:web:03cabb5eaea9707c86f2bd",
+    measurementId: "G-TGE4W3YEQN"
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Load saved notes
+const docRef = db.collection('notes').doc('personal');
+docRef.get().then((doc) => {
+  if (doc.exists) {
+    document.getElementById('notes').value = doc.data().content;
+  }
+});
+
+function showsav() {
+  document.getElementById('icon').style.display = 'block';
+}
+
+function hidesav() {
+  document.getElementById('ivon').style.display = 'none';
+}
+
+let debounceTimer;
+const debounceDelay = 1000; // Delay in milliseconds
+
+const save = (event) => {
+  console.log('Text changed: ', event.target.value);
+  docRef.set({
+    content: document.getElementById('notes').value
+  });
+};
+
+const debounce = (func, delay) => {
+  return function(event) {
+    clearTimeout(debounceTimer);
+    document.getElementById('icon').style.display = 'none';
+    debounceTimer = setTimeout(() => {
+        func(event);
+        document.getElementById('icon').style.display = 'block';
+    }, delay);
+  };
+};
+
+const debouncedSave = debounce(save, debounceDelay);
+
+const notesTextarea = document.getElementById('notes');
+notesTextarea.addEventListener('input', debouncedSave);
+notesTextarea.addEventListener('keyup', debouncedSave);
