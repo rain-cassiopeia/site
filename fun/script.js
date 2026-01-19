89f014c57fd788c8d3e8a33cf8650b3ff7237e81
Rain White <raincassiopeiawhite@gmail.com>
2026-01-19 16:57:04 -0500

    last push

--- fun/script.js
+++ fun/script.js
@@ -1,28 +1,29 @@
 function getRandomPastelColor() {
-    const maxHue = 360; 
+    const maxHue = 360;
     const minSaturation = 30;
-    const maxSaturation = 70; 
+    const maxSaturation = 70;
     const minLightness = 70;
-    const maxLightness = 90; 
-  
+    const maxLightness = 90;
+
     const hue = Math.floor(Math.random() * maxHue);
     const saturation = Math.floor(Math.random() * (maxSaturation - minSaturation) + minSaturation);
     const lightness = Math.floor(Math.random() * (maxLightness - minLightness) + minLightness);
-  
+
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
       }
-  
+
   // Function to change button color
   function changeButtonColor(buttonId) {
     const button = document.getElementById(buttonId);
     button.style.backgroundColor = getRandomPastelColor();
   }
-  
+
   // Set interval to change button colors every 2 seconds
   setInterval(() => {
     changeButtonColor('1');
     changeButtonColor('2');
     changeButtonColor('3');
     changeButtonColor('4');
-    
+    changeButtonColor('5');
+
   }, 2000);
